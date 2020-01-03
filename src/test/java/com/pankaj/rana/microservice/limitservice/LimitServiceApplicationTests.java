485a3eaefcfb1b8ea215b054013faacf4c0e9b79
pankajrana737 <pankajrana737@gmail.com>
2020-01-03 13:19:33 +0530

    new file:   .gitignore 	new file:   .mvn/wrapper/MavenWrapperDownloader.java 	new file:   .mvn/wrapper/maven-wrapper.jar 	new file:   .mvn/wrapper/maven-wrapper.properties 	new file:   mvnw 	new file:   mvnw.cmd 	new file:   pom.xml 	new file:   src/main/java/com/pankaj/rana/microservice/limitservice/LimitServiceApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/com/pankaj/rana/microservice/limitservice/LimitServiceApplicationTests.java

---
+++ src/test/java/com/pankaj/rana/microservice/limitservice/LimitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pankaj.rana.microservice.limitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
